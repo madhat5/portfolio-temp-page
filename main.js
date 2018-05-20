5ce918dfb8039c904e246d8bed08777750e07e4f
David Malary <davidmalary@Davids-MacBook-Pro.local>
2018-05-20 13:23:21 -0400

    animation builds

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
 console.log('sim sim salabim')
+
+
+// change div display
+var inlineBlock = function() {
+  $('.letter').css('display', 'inline-block');
+  console.log('display inline block');
+};
+inlineBlock();
+
+// // display block
+// var block = function() {
+//   $('.letter').css('display', 'block');
+//   console.log('display block');
+// };
+// setTimeout(block, 3000);
+//
+// // hide div classes
+// var hideLetter = function() {
+//   $('#letter2, #letter3, #letter4, #letter5, #letter5').css('display', 'none');
+//   console.log('hiding letter');
+// };
+// setTimeout(hideLetter, 3500)
+//
+// // change text content
+// var changeLetter = function() {
+//   $('#letter1').text('U')
+//   console.log('change letter');
+// }
+// setTimeout(changeLetter, 4000);
+
+
+
+// Create add/remove classes functions
+// classes have animations associated with themd
+var addFade = function() {
+  $('.letter2', '.letter3', '.letter4', '.letter5').addClass('letterFade');
+};
+
+var timeoutDelay = function () {
+  setTimeout( addFade, 3000);
+  console.log('fading');
+};
+timeoutDelay();
